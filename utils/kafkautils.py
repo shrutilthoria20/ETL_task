42d93b2bbd381d64abfdb9920c7a7c0bf1652291
shrutil thoria <shrutilthoria50@gmail.com>
2024-04-15 14:30:11 +0530

    edit kafkautils

--- utils/kafkautils.py
+++ utils/kafkautils.py
@@ -1,12 +1,27 @@
+import os
+from confluent_kafka import Producer, Consumer
+
 class KafkaUtils:
     def __init__(self):
-        pass
+        self.bootstrap_servers = os.environ['BOOTSTRAP_SERVER']
 
-    def create_connection(self):
-        pass
+    def produce_data(self,topic,value):
+        # Kafka producer configuration
+        producer_config = {
+            'bootstrap.servers': self.bootstrap_servers
+        }
+        # Create Kafka producer
+        producer = Producer(producer_config)
+        producer.produce(topic, value=value)
+        producer.flush()
 
-    def create_producer(self):
-        pass
-
-    def flush(self):
-        pass
+    def data_consumer(self):
+        # Kafka consumer configuration
+        consumer_config = {
+            'bootstrap.servers': self.bootstrap_servers,
+            'group.id': 'my_consumer_group',
+            'auto.offset.reset': 'earliest'  # Start reading from the beginning of the topic
+        }
+        # Create Kafka consumer
+        consumer = Consumer(consumer_config)
+        return consumer
