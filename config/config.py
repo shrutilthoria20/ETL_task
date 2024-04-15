1fd444b4c0649a8834db8df55d87db6ed0e13da5
shrutil thoria <shrutilthoria50@gmail.com>
2024-04-15 14:53:10 +0530

    edit kafkautils

--- config/config.py
+++ config/config.py
@@ -1,6 +1,28 @@
 class Config:
     def __init__(self):
         pass
-    def schema(self):
-        avro_schema={}
+
+    def all_schema(self):
+        avro_schema = {
+            "type": "record",
+            "name": "Request",
+            "fields": [
+                {"name": "IP", "type": "string"},
+                {"name": "Time", "type": "string"},
+                {"name": "URL", "type": {"type": "array", "items": {
+                    "type": "record",
+                    "name": "URLComponents",
+                    "fields": [
+                        {"name": "Scheme", "type": "string"},
+                        {"name": "Netloc", "type": "string"},
+                        {"name": "Path", "type": "string"},
+                        {"name": "Params", "type": "string"},
+                        {"name": "Query", "type": "string"},
+                        {"name": "Fragment", "type": "string"}
+                    ]
+                }}},
+                {"name": "Status", "type": "string"}
+            ]
+        }
         return avro_schema
+
---
+++ main.py
@@ -0,0 +1,83 @@
+import pandas as pd
+from fastavro import writer, parse_schema, reader
+import io
+from confluent_kafka import Producer, Consumer, KafkaError, KafkaException
+import fastavro
+from urllib.parse import urlparse
+import pymongo
+from config.config import Config
+
+
+class Etl:
+
+    def __init__(self):
+        self.config_instance = Config()
+    def parse_url(self,url):
+        parsed_url = urlparse(url)
+        url_components = {
+            "Scheme": parsed_url.scheme,
+            "Netloc": parsed_url.netloc,
+            "Path": parsed_url.path,
+            "Params": parsed_url.params,
+            "Query": parsed_url.query,
+            "Fragment": parsed_url.fragment
+        }
+        return [url_components]
+    def read_csv_file(self, csv_file_path):
+        # Open the CSV file
+        df = pd.read_csv(csv_file_path)
+
+        # Print the DataFrame to see its contents
+        return df
+
+    def parse_data(self,df):
+        df['URL'] = df['URL'].apply(self.parse_url)
+
+        ip_pattern = r"\b(?:\d{1,3}\.){3}\d{1,3}\b"
+        valid_ips = df['IP'].str.match(ip_pattern)
+        df = df[valid_ips]
+
+        df = df[['IP', 'Time', 'URL', 'Status']]
+        df['Time'] = df['Time'].str.replace('[', '').str.strip()
+        df['Time'] = pd.to_datetime(df['Time'], format='%d/%b/%Y:%H:%M:%S', errors='coerce')
+        # print(df['IP'],df['Time'],df['URL'],df['Status'])
+        return df
+
+    def create_avro_file(self):
+        # print("Creating Avro formate file")
+        # df['IP'] = df['IP'].astype(str)
+        # df['Time'] = df['Time'].astype(str)
+        # df['URL'] = df['URL'].astype(dict)
+        # df['Status'] = df['Status'].astype(str)
+        # df['URL'] = df['URL'].apply(self.parse_url)
+
+
+        avro_schema = self.config_instance.all_schema()
+
+
+        # Convert DataFrame to list of dictionaries
+        # records = df.to_dict(orient='records')
+        # for i in records:
+        #     print(i['URL'])
+        # Convert schema to parsed schema
+        # parsed_schema = parse_schema(avro_schema)
+
+        # with io.BytesIO() as avro_file:
+        #     writer(avro_file, parsed_schema, records)
+
+            # Reset file pointer
+            # avro_file.seek(0)
+
+            # Get the Avro bytes
+            # avro_bytes = avro_file.read()
+
+        # Now, you can save the avro_bytes to a file or use it as needed
+        # For example, you can write it to a file like this:
+        # with open("data.avro", "wb") as f:
+        #     f.write(avro_bytes)
+
+if __name__ == '__main__':
+    obj = Etl()
+    # df = obj.read_csv_file(r"E:\archive\weblog.csv")
+    # obj.parse_data(df)
+    obj.create_avro_file()
