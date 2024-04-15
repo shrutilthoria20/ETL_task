264a6ffc3ab52306df75c39ba6daeef09b09e6b0
shrutil thoria <shrutilthoria50@gmail.com>
2024-04-15 14:15:42 +0530

    edit mongoutils

--- utils/mongoutils.py
+++ utils/mongoutils.py
@@ -1,20 +1,18 @@
 import pymongo
 import os
+
+
 class MongoUtils:
     def __init__(self):
         pass
 
-    def create_connection(self,db_name,collection_name):
+    def create_connection(self, db_name, collection_name):
         mongo_uri = os.environ['MONGO_URL']
         client = pymongo.MongoClient(mongo_uri)
         db = client[db_name]
         collection = db[collection_name]
         return collection
 
-
-    def insert_data(self,collection,data):
+    def insert_data(self, collection, data):
         collection.insert_many(data)
 
-obj=MongoUtils
-
-print(os.environ['BOOTSTRAP_SERVER'])
